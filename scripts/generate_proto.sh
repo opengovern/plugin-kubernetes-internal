ca858ea7e5877dbb05707971203e51f6c0ef236c
artaasadi <arta-c@kaytu.io>
2024-05-31 12:57:51 +0200

    fix: add grpc proto file

---
+++ scripts/generate_proto.sh
@@ -0,0 +1,5 @@
+protoc --go_out=plugin/proto/src/golang --go_opt=paths=source_relative \
+    --go-grpc_out=plugin/proto/src/golang --go-grpc_opt=paths=source_relative \
+    plugin/proto/*.proto
+mv plugin/proto/src/golang/plugin/proto/* plugin/proto/src/golang/
+rm -rf plugin/proto/src/golang/plugin
