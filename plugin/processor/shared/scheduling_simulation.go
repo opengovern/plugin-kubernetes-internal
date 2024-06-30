91a29020c45de89d4fc8b5319b577b2f56e31c04
Saleh Khazaei <saleh.khazaei@gmail.com>
2024-06-30 16:31:01 +0400

    fix: Fixes scheduling

--- plugin/processor/shared/scheduling_simulation.go
+++ plugin/processor/shared/scheduling_simulation.go
@@ -46,17 +46,108 @@ func (s *Scheduler) AddPodDisruptionBudget(pdb policyv1.PodDisruptionBudget) {
 	s.pdbs = append(s.pdbs, pdb)
 }
 
+func (s *Scheduler) AddDaemonSet(item v1.DaemonSet) {
+	for _, node := range s.nodes {
+		if s.canScheduleOnNode(item.Spec.Template.Spec, &node) {
+			s.schedulePod(item.Spec.Template, &node)
+		}
+	}
+}
+
+func (s *Scheduler) AddDeployment(item v1.Deployment) {
+	for i := 0; i < int(*item.Spec.Replicas); i++ {
+		s.schedulePodWithStrategy(item.Spec.Template)
+	}
+}
+
+func (s *Scheduler) AddJob(item v12.Job) {
+	for i := 0; i < int(*item.Spec.Completions); i++ {
+		s.schedulePodWithStrategy(item.Spec.Template)
+	}
+}
+
+func (s *Scheduler) AddStatefulSet(item v1.StatefulSet) {
+	for i := 0; i < int(*item.Spec.Replicas); i++ {
+		s.schedulePodWithStrategy(item.Spec.Template)
+	}
+}
+
+func (s *Scheduler) AddPod(item v13.Pod) {
+	s.schedulePodWithStrategy(v13.PodTemplateSpec{
+		ObjectMeta: item.ObjectMeta,
+		Spec:       item.Spec,
+	})
+}
+
+func (s *Scheduler) GetNodeUtilization() map[string]map[string]float64 {
+	utilization := make(map[string]map[string]float64)
+	for _, node := range s.nodes {
+		utilization[node.Name] = map[string]float64{
+			"CPU":    node.AllocatedCPU / (node.VCores * HeadroomFactor),
+			"Memory": node.AllocatedMem / (node.Memory * HeadroomFactor),
+			"Pods":   float64(node.AllocatedPod) / (float64(node.MaxPodCount) * PodHeadroomFactor),
+		}
+	}
+	return utilization
+}
+
+func (s *Scheduler) CanRemoveNode(nodeName string) (bool, error) {
+	var nodeToRemove *KubernetesNode
+	for i, node := range s.nodes {
+		if node.Name == nodeName {
+			nodeToRemove = &s.nodes[i]
+			break
+		}
+	}
+
+	if nodeToRemove == nil {
+		return false, fmt.Errorf("node %s not found", nodeName)
+	}
+
+	// Check if the node is already empty
+	if len(nodeToRemove.Pods) == 0 {
+		return true, nil
+	}
+
+	// Create a temporary scheduler for simulation
+	tempNodes := make([]KubernetesNode, 0, len(s.nodes)-1)
+	for _, node := range s.nodes {
+		if node.Name != nodeName {
+			tempNodes = append(tempNodes, node)
+		}
+	}
+	tempScheduler := New(tempNodes)
+	tempScheduler.pdbs = s.pdbs // Copy PodDisruptionBudgets
+
+	// Simulate draining the node
+	for _, pod := range nodeToRemove.Pods {
+		if !tempScheduler.canEvictPod(pod) {
+			return false, nil
+		}
+
+		if !tempScheduler.schedulePodWithStrategy(pod) {
+			return false, nil
+		}
+	}
+
+	return true, nil
+}
+
 func (s *Scheduler) schedulePodWithStrategy(podSpec v13.PodTemplateSpec) bool {
 	// Sort nodes by most allocated resources
 	sort.Slice(s.nodes, func(i, j int) bool {
-		allocRatioI := (s.nodes[i].AllocatedCPU / (s.nodes[i].VCores * HeadroomFactor)) + (s.nodes[i].AllocatedMem / (s.nodes[i].Memory * HeadroomFactor))
-		allocRatioJ := (s.nodes[j].AllocatedCPU / (s.nodes[j].VCores * HeadroomFactor)) + (s.nodes[j].AllocatedMem / (s.nodes[j].Memory * HeadroomFactor))
-		return allocRatioI > allocRatioJ
+		allocCpuRatioI := s.nodes[i].AllocatedCPU / (s.nodes[i].VCores * HeadroomFactor)
+		allocMemoryRatioI := s.nodes[i].AllocatedMem / (s.nodes[i].Memory * HeadroomFactor)
+
+		allocCpuRatioJ := s.nodes[j].AllocatedCPU / (s.nodes[j].VCores * HeadroomFactor)
+		allocMemoryRatioJ := s.nodes[j].AllocatedMem / (s.nodes[j].Memory * HeadroomFactor)
+
+		return min(allocCpuRatioI, allocMemoryRatioI) > min(allocCpuRatioJ, allocMemoryRatioJ)
 	})
 
 	// Try to schedule on the most allocated node that can accommodate the pod
 	for i := range s.nodes {
-		if s.canScheduleOnNode(podSpec.Spec, s.nodes[i]) {
+		if s.canScheduleOnNode(podSpec.Spec, &s.nodes[i]) {
 			s.schedulePod(podSpec, &s.nodes[i])
 			return true
 		}
@@ -65,7 +156,7 @@ func (s *Scheduler) schedulePodWithStrategy(podSpec v13.PodTemplateSpec) bool {
 	return false
 }
 
-func (s *Scheduler) canScheduleOnNode(podSpec v13.PodSpec, node KubernetesNode) bool {
+func (s *Scheduler) canScheduleOnNode(podSpec v13.PodSpec, node *KubernetesNode) bool {
 	// Check resources
 	if !s.hasEnoughResources(podSpec, node) {
 		return false
@@ -84,7 +175,7 @@ func (s *Scheduler) canScheduleOnNode(podSpec v13.PodSpec, node KubernetesNode) 
 	}
 
 	// Check pod affinity and anti-affinity
-	if !s.satisfiesAffinityRules(podSpec, node) {
+	if !s.satisfiesAffinityRules(podSpec, *node) {
 		return false
 	}
 
@@ -150,40 +241,7 @@ func (s *Scheduler) satisfiesPodAffinityTerm(term v13.PodAffinityTerm, node Kube
 	return false
 }
 
-func (s *Scheduler) AddDaemonSet(item v1.DaemonSet) {
-	for _, node := range s.nodes {
-		if s.canScheduleOnNode(item.Spec.Template.Spec, node) {
-			s.schedulePod(item.Spec.Template, &node)
-		}
-	}
-}
-
-func (s *Scheduler) AddDeployment(item v1.Deployment) {
-	for i := 0; i < int(*item.Spec.Replicas); i++ {
-		s.schedulePodWithStrategy(item.Spec.Template)
-	}
-}
-
-func (s *Scheduler) AddJob(item v12.Job) {
-	for i := 0; i < int(*item.Spec.Completions); i++ {
-		s.schedulePodWithStrategy(item.Spec.Template)
-	}
-}
-
-func (s *Scheduler) AddStatefulSet(item v1.StatefulSet) {
-	for i := 0; i < int(*item.Spec.Replicas); i++ {
-		s.schedulePodWithStrategy(item.Spec.Template)
-	}
-}
-
-func (s *Scheduler) AddPod(item v13.Pod) {
-	s.schedulePodWithStrategy(v13.PodTemplateSpec{
-		ObjectMeta: item.ObjectMeta,
-		Spec:       item.Spec,
-	})
-}
-
-func (s *Scheduler) hasEnoughResources(podSpec v13.PodSpec, node KubernetesNode) bool {
+func (s *Scheduler) hasEnoughResources(podSpec v13.PodSpec, node *KubernetesNode) bool {
 	cpuReq, memReq := s.getPodResourceRequests(podSpec)
 	return node.AllocatedCPU+cpuReq <= node.VCores*HeadroomFactor &&
 		node.AllocatedMem+memReq <= node.Memory*HeadroomFactor &&
@@ -273,60 +331,6 @@ func contains(slice []string, s string) bool {
 		}
 	}
 	return false
-}
-
-func (s *Scheduler) GetNodeUtilization() map[string]map[string]float64 {
-	utilization := make(map[string]map[string]float64)
-	for _, node := range s.nodes {
-		utilization[node.Name] = map[string]float64{
-			"CPU":    node.AllocatedCPU / (node.VCores * HeadroomFactor),
-			"Memory": node.AllocatedMem / (node.Memory * HeadroomFactor),
-			"Pods":   float64(node.AllocatedPod) / (float64(node.MaxPodCount) * PodHeadroomFactor),
-		}
-	}
-	return utilization
-}
-
-func (s *Scheduler) CanRemoveNode(nodeName string) (bool, error) {
-	var nodeToRemove *KubernetesNode
-	for i, node := range s.nodes {
-		if node.Name == nodeName {
-			nodeToRemove = &s.nodes[i]
-			break
-		}
-	}
-
-	if nodeToRemove == nil {
-		return false, fmt.Errorf("node %s not found", nodeName)
-	}
-
-	// Check if the node is already empty
-	if len(nodeToRemove.Pods) == 0 {
-		return true, nil
-	}
-
-	// Create a temporary scheduler for simulation
-	tempNodes := make([]KubernetesNode, 0, len(s.nodes)-1)
-	for _, node := range s.nodes {
-		if node.Name != nodeName {
-			tempNodes = append(tempNodes, node)
-		}
-	}
-	tempScheduler := New(tempNodes)
-	tempScheduler.pdbs = s.pdbs // Copy PodDisruptionBudgets
-
-	// Simulate draining the node
-	for _, pod := range nodeToRemove.Pods {
-		if !tempScheduler.canEvictPod(pod) {
-			return false, nil
-		}
-
-		if !tempScheduler.schedulePodWithStrategy(pod) {
-			return false, nil
-		}
-	}
-
-	return true, nil
 }
 
 func (s *Scheduler) canEvictPod(pod v13.PodTemplateSpec) bool {
